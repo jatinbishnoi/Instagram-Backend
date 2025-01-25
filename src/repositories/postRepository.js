642cd5c2680515b42e16d6fc83fb8b9cac60cf51
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-01-26 01:05:23 +0530

    completed  the project

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -1,4 +1,5 @@
 import Post from "../schema/post.js";
+
 export const createPost = async (caption, image, user) => {
     try {
         const newPost = await Post.create({ caption, image, user });
@@ -9,14 +10,25 @@ export const createPost = async (caption, image, user) => {
         console.log(error);
     }
 }
-export const findAllPosts = async () => {
+
+export const findAllPosts = async (offset, limit) => {
     try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1}).skip(offset).limit(limit).populate('user', 'username email _id');
         return posts;
     } catch(error) {
         console.log(error);
     }
 }
+
+export const countAllPosts = async () => {
+    try {
+        const count = await Post.countDocuments();
+        return count;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 export const findPostById = async (id) => {
     try {
         const post = await Post.findById(id);
@@ -25,9 +37,19 @@ export const findPostById = async (id) => {
         console.log(error);
     }
 }
+
 export const deletePostById = async (id) => {
     try {
         const post = await Post.findByIdAndDelete(id);
+        return post;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const updatePostById = async (id, updateObject) => {
+    try {
+        const post = await Post.findByIdAndUpdate(id, updateObject, { new: true });
         return post;
     } catch(error) {
         console.log(error);
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,4 +1,5 @@
-import User from '../schema/user';
+import User from '../schema/user.js';
+
 export const findUserByEmail = async (email) => {
     try {
        const user = await User.findOne({ email });
@@ -7,11 +8,22 @@ export const findUserByEmail = async (email) => {
         console.log(error);
     }
 }
+
 export const findAllUsers = async () => {
     try {
         const users = await User.find();
         return users;
     } catch(error) {
         console.log(error);
+    }
+}
+
+export const createUser = async (user) => {
+    try {
+        const newUser = await User.create(user);
+        return newUser;
+    } catch(error) {
+        console.log(error);
+        throw error;
     }
 }
